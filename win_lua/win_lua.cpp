1e47109b9c1b4ed7a7e976fbe583ed5268810d01
Dmitri Skliarov <dskliarov@gmail.com>
2016-02-17 22:03:44 -0500

    initial commit

---
+++ win_lua/win_lua.cpp
@@ -0,0 +1,12 @@
+// win_lua.cpp : Defines the entry point for the console application.
+//
+#include "stdafx.h"
+#include <lua.hpp>
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	lua_State *L = luaL_newstate();
+	return 0;
+
+}
+
